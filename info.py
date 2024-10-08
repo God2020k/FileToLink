1fc3e4bea4bf62d3dd792808c3176b8f3dcaf05a
God2020k <107332649+God2020k@users.noreply.github.com>
2024-10-08 07:39:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,9 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '17627887'))
+API_HASH = environ.get('API_HASH', 'd14e54b0791ca1f8b0f26786439e336e')
+BOT_TOKEN = environ.get('BOT_TOKEN', "5420856904:AAFHQtA3f7A9mn_c034bAPPb7I_af5JD8Eo")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
@@ -24,11 +24,11 @@ URL = environ.get("URL", "")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5211097531').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Test")
 
 # Shortlink Info
 SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
